264996c0f7191c95c522c868346de8f51dc7e9fc
Jerrick Rowe <5275899+JerrickRowe@users.noreply.github.com>
2018-12-27 23:11:41 +0800

    Add files via upload

---
+++ ANSI_ColorCode.h
@@ -0,0 +1,84 @@
+#ifndef _COLOR_CODE_H_
+#define _COLOR_CODE_H_
+
+//Regular
+#define R_D		"\x1B[0;30m"	//Dark = Black
+#define R_R		"\x1B[0;31m"	//Red
+#define R_G		"\x1B[0;32m"	//Green
+#define R_Y		"\x1B[0;33m"	//Yellow
+#define R_B		"\x1B[0;34m"	//Blue
+#define R_P		"\x1B[0;35m"	//Purple
+#define R_C		"\x1B[0;36m"	//Cyan
+#define R_W		"\x1B[0;37m"	//White
+#define R_F		"\x1B[0;39m"	//deFault
+
+//Bold
+#define B_D		"\x1B[1;30m"	
+#define B_R		"\x1B[1;31m"	
+#define B_G		"\x1B[1;32m"	
+#define B_Y		"\x1B[1;33m"	
+#define B_B		"\x1B[1;34m"	
+#define B_P		"\x1B[1;35m"	
+#define B_C		"\x1B[1;36m"	
+#define B_W		"\x1B[1;37m"	
+#define B_F		"\x1B[1;39m"
+
+//Underline
+#define U_D		"\x1B[4;30m"	
+#define U_R		"\x1B[4;31m"	
+#define U_G		"\x1B[4;32m"	
+#define U_Y		"\x1B[4;33m"	
+#define U_B		"\x1B[4;34m"	
+#define U_P		"\x1B[4;35m"	
+#define U_C		"\x1B[4;36m"	
+#define U_W		"\x1B[4;37m"	
+#define U_F		"\x1B[4;39m"	
+
+//Background
+#define BG_D	"\x1B[40m"	
+#define BG_R	"\x1B[41m"	
+#define BG_G	"\x1B[42m"	
+#define BG_Y	"\x1B[43m"	
+#define BG_B	"\x1B[44m"	
+#define BG_P	"\x1B[45m"	
+#define BG_C	"\x1B[46m"	
+#define BG_W	"\x1B[47m"	
+#define BG_F	"\x1B[49m"	
+
+//High Intensty
+#define H_D		"\x1B[0;90m"	
+#define H_R		"\x1B[0;91m"	
+#define H_G		"\x1B[0;92m"	
+#define H_Y		"\x1B[0;93m"	
+#define H_B		"\x1B[0;94m"	
+#define H_P		"\x1B[0;95m"	
+#define H_C		"\x1B[0;96m"	
+#define H_W		"\x1B[0;97m"	
+#define H_F		"\x1B[0;99m"	
+
+//Bold High Intensty
+#define BH_D	"\x1B[1;90m"	
+#define BH_R	"\x1B[1;91m"	
+#define BH_G	"\x1B[1;92m"	
+#define BH_Y	"\x1B[1;93m"	
+#define BH_B	"\x1B[1;94m"	
+#define BH_P	"\x1B[1;95m"	
+#define BH_C	"\x1B[1;96m"	
+#define BH_W	"\x1B[1;97m"	
+#define BH_F	"\x1B[1;99m"	
+
+//High Intensty backgrounds
+#define BGH_D	"\x1B[0;100m"	
+#define BGH_R	"\x1B[0;101m"	
+#define BGH_G	"\x1B[0;102m"	
+#define BGH_Y	"\x1B[0;103m"	
+#define BGH_B	"\x1B[0;104m"	
+#define BGH_P	"\x1B[0;105m"	
+#define BGH_C	"\x1B[0;106m"	
+#define BGH_W	"\x1B[0;107m"	
+#define BGH_F	"\x1B[0;109m"	
+
+#define C_Rst		"\x1B[0m"	//Color reset
+#endif
+
+
